7c1ee310b198a9d82e336459709b1c8aee0713b9
181847 <753090084@qq.com>
2017-11-27 13:20:21 +0800

    增加一个公用的初始化lua脚本

---
+++ Lua/Init.lua
@@ -0,0 +1,51 @@
+--[[
+This script is used to config the interpretoer,
+such as adding the search patch.
+--]]
+print("Start Initialization")
+
+function DebugLogger(message, ...)
+    print('----- DEBUG:', message, ...)
+end
+
+-- AddPath to add a path to the search for loading file
+local function AddPath(path)
+    package.path = package.path..";"..path
+end
+
+local function AddCPath(path)
+    package.cpath = package.cpath..";"..path
+end
+
+-- print the key, value in a table
+function PrintTable(t)
+    for k, v in pairs(t) do print(k, v) end
+end
+
+AddPath("D:\\GitHub\\Lua\\LoadAssets\\?.lua")
+-- AddPath("D:\\GitHub\\Lua\\LoadAssets\\?.dll")
+AddPath(".\\luaScript\\?.lua")
+
+-- store the original 'loadfile', replace it with loadfileInPath
+old_loadfile = loadfile
+
+-- modify the loadfile function,
+-- enable the function the ability to search through the package.path, 
+-- not just in current folder.
+-- notice that if the fileName end with ".lua", 
+-- ".lua" will be removed.
+loadfile = function(filePath)
+    -- remove the ".lua" in the end.
+    fileName = string.match(filePath, "(%w-).lua$") or filePath
+    
+    -- use the searchpath to find the actual path for the file
+    fileName = package.searchpath(fileName, package.path)
+    
+    if fileName then
+        return old_loadfile(fileName)
+    else
+        error("no such file:\n\t"..filePath)
+    end
+end
+
+print("Initialization Completed")
