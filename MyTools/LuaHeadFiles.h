e8924a6ab2c04f24b15e7ec80ebd3709ca8360f1
181847 <753090084@qq.com>
2017-11-27 12:46:20 +0800

    在实现lua的多线程时候对lua的源文件进行过修改，在头文件中增加定义，决定使用哪一个版本的lua。码

--- MyTools/LuaHeadFiles.h
+++ MyTools/LuaHeadFiles.h
@@ -3,9 +3,16 @@
 extern "C"
 {
 #endif
+
+#ifndef THREAD_SAFTY
 #include "../Lua/Lua-5.3.4/src/lua.h"
 #include "../Lua/Lua-5.3.4/src/lauxlib.h"
 #include "../Lua/Lua-5.3.4/src/lualib.h"
+#elif THREAD_SAFTY
+#include "../Lua/Lua-5.3.4/srcThreadSafty/lua.h"
+#include "../Lua/Lua-5.3.4/srcThreadSafty/lauxlib.h"
+#include "../Lua/Lua-5.3.4/srcThreadSafty/lualib.h"
+#endif
 #ifdef __cplusplus
 }
 #endif
