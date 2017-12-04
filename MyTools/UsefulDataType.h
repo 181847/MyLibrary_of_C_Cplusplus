55ba347ec5610d3d11dd859c1518611db46a5fbe
181847 <753090084@qq.com>
2017-12-04 19:01:33 +0800

    添加两个新的有用的类型别名

--- MyTools/UsefulDataType.h
+++ MyTools/UsefulDataType.h
@@ -1,7 +1,12 @@
 // this file define the useful data type such as BYTE, WORD , DWORD
-
+#pragma once
 typedef unsigned char	BYTE;
 typedef unsigned short	WORD;
 typedef unsigned long	DWORD;
 
 typedef unsigned int UINT;
+
+
+
+typedef unsigned char u8;
+typedef unsigned long u32;
