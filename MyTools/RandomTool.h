b7bea15a0002e7947b80d260f5d3d27deea87aef
181847 <753090084@qq.com>
2017-12-06 20:29:21 +0800

    将RandomTools重命名为RandomTool，并且修改命名空间，删除最后的s。

--- MyTools/RandomTool.h
+++ MyTools/RandomTool.h
@@ -1,17 +1,19 @@
 #pragma once
 #include <vector>
+#include "MathTool.h"
 // this head file declare some useful function
 // to generate the random numbers
 
-namespace randomTool
+namespace RandomTool
 {
 
-
+// this function to generate the random order of 0~(length-1)
 template<typename T>
 inline void RandomSequence(size_t length, std::vector<T> * psequence, unsigned int seed = 1)
 {
 	auto & sequence = *psequence;
 	srand(seed);
+	sequence.clear();
 	sequence.resize(length);
 	for (size_t i = 0; i < length; ++i)
 	{
@@ -23,6 +25,25 @@ inline void RandomSequence(size_t length, std::vector<T> * psequence, unsigned i
 		int randIndex = rand() % length;
 		
 		std::swap(sequence[i], sequence[randIndex]);
+	}
+}
+
+template<typename T>
+inline void RandomNumbers(
+	size_t length,
+	std::vector<T> * pcontainer,
+	T max,
+	T min = 0,
+	unsigned int seed = 1)
+{
+	auto & container = *pcontainer;
+	srand(seed);
+	
+	container.clear();
+	container.resize(length);
+	for (size_t i = 0; i < length; ++i)
+	{
+		container[i] = MathTool::clamp<T>(rand() % max, min, max);
 	}
 }
 
