c2cb33dd3a58abdac7827cf6cd2e19a74aa8d3c2
181847 <753090084@qq.com>
2018-01-17 21:26:10 +0800

    修改类型ID生成器，添加一个IDOf函数。
    之前本来想要用newID和getID来限制程序员不要乱生成类型ID，但是发现在编译的时候很难控制函数的调用顺序，所以干脆取消这个newID和getID的功能，现在先用另一个函数来替代他们的功能，等其他地方没有问题了再来删除这两个函数。

--- MyTools/TypeTool.h
+++ MyTools/TypeTool.h
@@ -41,14 +41,14 @@ public:
 	template<typename TYPE>
 	inline static size_t newID()
 	{
-		static size_t idOfTheType = IDContainer<TYPE>(++distribID);
+		static const size_t idOfTheType = IDContainer<TYPE>(++distribID);
 		return idOfTheType;
 	}
 
 	// Get the id of the Type,
 	// if the Type is new, unlike the newID(),
 	// it won't generate any new ID, and will return zero all the time
-	// with the same Type(event you call newID() later)
+	// with the same Type(even you call newID() later)
 	// so this mechanism can make the user clearly understand 
 	// whether they want to generate newID, 
 	// or just to try a Type
@@ -59,6 +59,13 @@ public:
 	inline static size_t getID()
 	{
 		static size_t idOfTheType = IDContainer<TYPE>(0);
+		return idOfTheType;
+	}
+
+	template<typename TYPE>
+	inline static size_t IDOf()
+	{
+		static const size_t idOfTheType = IDContainer<TYPE>(++distribID);
 		return idOfTheType;
 	}
 };
