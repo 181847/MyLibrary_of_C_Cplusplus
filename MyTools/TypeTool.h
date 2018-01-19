c7d13806303b877bcfd0d1968d4c8c67e3a92625
181847 <753090084@qq.com>
2018-01-19 17:41:58 +0800

    为TypeTool添加工具，判断类型是否属于某个类型集合中。
    IsOneOf判断一个类型，IsAllOf判断多个类型是否属于某个类型合集中。

--- MyTools/TypeTool.h
+++ MyTools/TypeTool.h
@@ -20,17 +20,108 @@ private:
 
 public:
 	template<typename TYPE>
-	inline static size_t IDOf()
+	inline static const size_t IDOf()
 	{
 		static const size_t idOfTheType = ++m_distribID;
 		return idOfTheType;
 	}
 };
-
-
+//||||||||||||||||||||||||||||||||||||||||||||||||||
 // initialize the series id of the specific Series type
 template<typename Series>
 size_t IDGenerator<Series>::m_distribID = 0;
 
+// BoolCondition used to return the bool based on the CONDITION value.
+template<bool CONDITION, bool THEM, bool ELSE>
+struct BoolCondition{};
+//||||||||||||||||||||||||||||||||||||||||||||||||||
+template<bool THEN, bool ELSE>
+struct BoolCondition<true, THEN, ELSE>
+{
+	enum { value = THEN };
+};
+//||||||||||||||||||||||||||||||||||||||||||||||||||
+template<bool THEN, bool ELSE>
+struct BoolCondition<false, THEN, ELSE>
+{
+	enum { value = ELSE };
+};
 
-} // namespace TypeIDTools
+// struct IsOneOf is used to check whether a type is in the list of types.
+// the first parameter is the type to be checked, rest are the typeList.
+// Usage: IsOneOf<int, float, char, double>::value => false
+//		  IsOneOf<int, float, char, int>::value	   => true.
+template<typename TYPE, typename ...TYPE_LIST>
+struct IsOneOf {};
+//||||||||||||||||||||||||||||||||||||||||||||||||||
+template<typename TYPE, typename FIRST, typename ...REST>
+struct IsOneOf<TYPE, FIRST, REST...>
+{
+	enum {
+		value =
+		BoolCondition<std::is_same<TYPE, FIRST>::value,
+		true,
+		IsOneOf<TYPE, REST...>::value>::value
+	};
+};
+//||||||||||||||||||||||||||||||||||||||||||||||||||
+template<typename TYPE>
+struct IsOneOf<TYPE>
+{
+	enum { value = false };
+};
+
+
+// TypeContainer to have a set of type to be checked.
+template<typename ...TYPE_LIST>
+struct TypeContainer{};
+//||||||||||||||||||||||||||||||||||||||||||||||||||
+// Seperate the set of types to the firstType,
+// and the rest to another TypeContainer.
+template<typename FIRST, typename ...REST>
+struct TypeContainer<FIRST, REST...>
+{
+	typedef FIRST firstType;
+	typedef TypeContainer<REST...> restTypeContainer;
+};
+//||||||||||||||||||||||||||||||||||||||||||||||||||
+// If there is only one type left, 
+// the restTypeContainer is the TypeContainer which have no template parameters.
+template<typename LAST>
+struct TypeContainer<LAST>
+{
+	typedef LAST firstType;
+	typedef TypeContainer<> restTypeContainer;
+};
+
+
+// IsAllOf is the extension of IsOneOf, 
+// It will check if all the types are in another typeList.
+// It MUST be used with typeContainer.
+// Usage:	IsAllOf< TypeContainer<int, char>,			double, int, char	>::value	=> true
+//			IsAllOf< TypeContainer<>,					double, int			>::value	=> true
+//			IsAllOf< TypeContainer<void>,				double, int			>::value	=> false
+//			IsAllOf< TypeContainer<void, double, int>						>::value	=> false
+//			IsAllOf<									int, float, double	>::value	=> compile error
+template<typename ...CHECK_ARGS>
+struct IsAllOf { static_assert(false, "Please use IsAllOf with TypeContainer."); };
+//||||||||||||||||||||||||||||||||||||||||||||||||||
+template<typename ...TYPE_LIST>
+struct IsAllOf<TypeContainer<>, TYPE_LIST...>
+{
+	enum { value = true };
+};
+//||||||||||||||||||||||||||||||||||||||||||||||||||
+template<typename ...TYPE_LIST_ALL, typename ...TYPE_LIST>
+struct IsAllOf<TypeContainer<TYPE_LIST_ALL...>, TYPE_LIST...>
+{
+	typedef TypeContainer<TYPE_LIST_ALL...> TYPE_CONTAINER;
+	
+	enum {
+		value = IsOneOf<TYPE_CONTAINER::firstType, TYPE_LIST...>::value 
+		&& IsAllOf<TYPE_CONTAINER::restTypeContainer, TYPE_LIST...>::value
+	};
+};
+
+
+} // namespace TypeTool
