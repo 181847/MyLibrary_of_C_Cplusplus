f34306b6ab21407fb4ff2fca0720af52d1187aa1
181847 <753090084@qq.com>
2018-02-28 21:34:18 +0800

    给MathTool添加一个浮点数相等判断的函数。

--- MyTools/MathTool.h
+++ MyTools/MathTool.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <algorithm>
 
 // this head file define some useful function for math
 
@@ -19,6 +20,19 @@ inline T clamp(const T& target, const T& min, const T& max)
 	{
 		return target;
 	}
+}
+
+// This function used to compare floating-point values for equality.
+// Refer to http://en.cppreference.com/w/cpp/types/numeric_limits/epsilon
+template<class T>
+typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type
+almost_equal(T x, T y, int ulp)
+{
+	// the machine epsilon has to be scaled to the magnitude of the values used
+	// and multiplied by the desired precision in ULPs (units in the last place)
+	return std::abs(x - y) <= std::numeric_limits<T>::epsilon() * std::abs(x + y) * ulp
+		// unless the result is subnormal
+		|| std::abs(x - y) < std::numeric_limits<T>::min();
 }
 
 }// namespace MathTool
