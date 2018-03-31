5f7afada61a7a922a1a131d56902d0a8fdedc3ea
181847 <753090084@qq.com>
2018-03-31 20:30:33 +0800

    删除无用的代码，简化测试模块定义，增加单元测试时间输出。

--- MyTools/UnitTestModules.h
+++ MyTools/UnitTestModules.h
@@ -3,192 +3,191 @@
 #include <string>
 #include <vector>
 #include <exception>
-#include "MyAssert.h"
+#include <assert.h>
+#include <chrono>
+#include <iostream>
 
-//---------------------------------------------------------------------------------
-// A simple example to use this modules.
-/*---------------------------------------------------------------------------------
-
-#include <MyTools\RandomTool.h>
-#include <MyTools\UnitTestModules.h>
-
-DECLARE_TEST_UNITS;
-
-namespace TestUnit
-{
-void GetReady() {}
-void AfterTest() {}
-
-void AddTestUnit()
-{
-#pragma region Try the testUnit
-	TEST_UNIT_START("a test always success")
-		return true;
-	TEST_UNIT_END;
-#pragma endregion
-
-}// AddTestUnit()
-
-}// namespace TestUnit
-
-int main()
-{
-	TestUnit::testMain();
-	return 0;
-}
-
-*///------------------------------------------------------------
+/********************************************.
+|                                            |
+.********************************************/
 
 
-#define EQ(a, b)	((a) == (b)) 
-#define GT(a, b)	((a) >  (b))
-#define LE(a, b)	((a) >  (b))
-#define GT_EQ(a, b) ((a) >  (b))
-#define LE_EQ(a, b) ((a) >  (b))
-#define FT_EQ(fa, fb) (1e-5 > std::abs((fa) - (fb)))
-#define NOT_EQ(a, b) ((a) != (b))
-
-#define NO_ERROR(errorCount) EQ(0, errorCount)
-
-#define CHECK_ARR_2(arr, COMPARE, e0, e1)\
-	(COMPARE(arr[0], e0) && COMPARE(arr[1], e1))
-
-#define CHECK_ARR_3(arr, COMPARE, e0, e1, e2) \
-	(COMPARE(arr[0], e0) && COMPARE(arr[1], e1) && COMPARE(arr[2], e3)
-
-/*
-** to use this module , please use DECLARE_TEST_UNITS; at the head
-** of the executed .cpp file, 
-** implements the 'void TestUnit::GetReady()'/'void TestUnit::AfterTest();'
-** implements the 'void TestUnit::AddTestUnit()', you can find more at
-** the comment of the 'void AddTestUnit()'.
+/*!
+	\brief MACRO HELP TO USE THIS MODULE 
 */
+#define TEST_MODULE_START \
+	namespace TestUnit\
+	{\
+		void AddTestUnit(std::vector<std::function<unsigned int(std::string& unitName)>>& appendToQuery)\
+		{
 
-extern std::vector<std::function<bool(std::string& unitName)>> testUnits;
+/*!
+	\brief MACRO HELP TO USE THIS MODULE 
+*/
+#define TEST_MODULE_END \
+		}\
+	}\
+	int main()\
+	{\
+		TestUnit::execute();\
+		return 0;\
+	}\
 
-extern unsigned int gTestUnitCount;
-extern unsigned int	gSuccessCount;
-
-
-// the marco start declare the units,
-// inside, the marco have declare an errorLogger,
-// which is used to log the error count inside the testUnit,
-// the user can use it to log the error count,
-// and return the errorLogger.conclusion(),
-// it will return true, if no error happened.
-#define DECLARE_TEST_UNITS											\
-	static std::vector<std::function<bool(std::string& unitName)>>	\
-	testUnits;														\
-	static unsigned int gTestUnitCount	= 0;						\
-	static unsigned int	gSuccessCount	= 0;
-
+/*!
+	\brief start an lambda and push it into the test query in which to run the target codes
+	the lambda need a string argument as the name of the unit.
+	return an int value for how many error has been recorded inside the test.
+	There for, in the tested code, you can return an integer manully for num errors.
+	However, for convenience, this macro also declare an special class 'ErrorLogger' who will help user to record errors,
+	and return error number at the end of lambda.
+*/
 #define TEST_UNIT_START(UnitName)			\
-	testUnits.push_back(std::move(			\
-		[](std::string& unitName) -> bool {	\
+	appendToQuery.push_back(std::move(			\
+		[](std::string& unitName) -> unsigned int {	\
 			unitName = UnitName;			\
 	TestUnit::ErrorLogger errorLogger;		// for each testUnit have a errorLogger to help log the errorCount.
 
+/*!
+	\brief stop declare an test unit
+	enclose the lambda delcared by marco 'TEST_UNIT_START'.
+*/
 #define TEST_UNIT_END \
 		return errorLogger.conclusion(); \
 	}))
 
 namespace TestUnit 
 {
+	/*!
+		\brief which unit to measure time elapsed.
+	*/
+	using DurationType = std::chrono::microseconds;
 
-// the user should implements this function 
-// to get some previous work for test.
-// such as initialize the tested variable.
-// there is no way to pass arguments between the functions,
-// so just use the global variable.
-void GetReady();
-void AfterTest();
+	/*!
+		\brief give the time unit a name to be printed in the output, e.g. "microseconds" or "seconds"...
+	*/
+	const std::string DURATION_TYPE_NAME = "microseconds";
 
-// user should implemment this function
-// to add the test units,
-// for simple using , just 
-// use the macro 'TEST_UNIT_START(UnitName)' and 
-// 'TEST_UNIT_END' to include the desired code.
-// Inside the two marco , there is a lambda,
-// the only thing that you will concen about is the return 
-// of the lambda.
-// The return value should be a boolean, which indicate
-// whether the unit test gSuccessCount or not.
-// For example:
-//		TEST_UNIT_START("a test will always gSuccessCount")
-//			...do the job...
-//			return true
-//		TEST_UNIT_END
-void AddTestUnit();
-inline void RunTest()
+/*!
+	\brief store brief test result such as test number and success number.
+*/
+struct TestResult
 {
-	std::string unitName;
+	unsigned int m_countTests = 0;
+	unsigned int m_countSuccess = 0;
+};
+
+/*!
+	\brief add test units into the array
+	\param appendToQuery append the test units into this array
+*/
+void AddTestUnit(std::vector<std::function<unsigned int(std::string& unitName)>>& appendToQuery);
+
+/*!
+	\brief run unit tests in the array'testUnits'
+	\param testUnits test to be run
+	\param result return brief result
+*/
+inline void RunTest(std::vector<std::function<unsigned int(std::string& unitName)>>& testUnits, TestResult& result)
+{
+	std::string		unitName;
+	unsigned int	errorCount;
+	bool			isSuccess = false;
+	DurationType	countTime;
+
 	for (auto & testUnit : testUnits)
 	{
-		bool result = testUnit(unitName);
-		gSuccessCount += result;
-		std::printf("\t%s\t\t\t%s\n", result ? "success" : "failed", unitName.c_str());
+		errorCount = 0;
+		try 
+		{
+			auto startTimePoint = std::chrono::system_clock::now();
+			errorCount = testUnit(unitName);
+			auto endTimePoint = std::chrono::system_clock::now();
+
+			countTime = std::chrono::duration_cast<DurationType>(endTimePoint - startTimePoint);
+		}
+		catch (std::exception e)
+		{
+			std::cout << "\t" << unitName << "exception happend: " << e.what() << std::endl;
+			continue;
+		}
+
+		isSuccess = ( errorCount == 0 );
+		if (isSuccess)
+		{
+			++result.m_countSuccess;
+		}
+
+		std::printf(
+			// success/failed	errorCount	costTime   unitName
+			"\t%s"              "\t%d"      "\t%s:%8lld"  "\t\t%s\n", 
+			isSuccess ? "success" : "failed", 
+			errorCount, 
+			DURATION_TYPE_NAME.c_str(), countTime.count(), 
+			unitName.c_str());
 	}
 }
 
-inline void Summary()
+/*!
+	\brief show result in the console
+	\param result the brief result returned by RunTest.
+*/
+inline void Summary(const TestResult& result)
 {
-	gTestUnitCount = testUnits.size();
 	std::printf("Summarize:\n");
 	std::printf("TestCount\tsuccess\t\tfailed\n");
-	std::printf("%d\t\t%d\t\t%d\n", gTestUnitCount, gSuccessCount, gTestUnitCount - gSuccessCount);
+	std::printf("%d\t\t%d\t\t%d\n", result.m_countTests, result.m_countSuccess, result.m_countTests - result.m_countSuccess);
 }
 
+/*!
+	\brief core function in which to add user-defined tests -> run them -> show summary
+	may be we can 
+*/
 inline void execute()
 {
-	GetReady();
-	AddTestUnit();
-	RunTest();
-	AfterTest();
-	Summary();
-}
+	std::vector<std::function<unsigned int(std::string& unitName)>> testUnitQuery;
+	AddTestUnit(testUnitQuery);
 
-inline void testMain()
-{
-	try
-	{
-		execute();
-	}
-	catch (std::exception & e)
-	{
-		std::fprintf(stderr, "ERROR:%s", e.what());
-	}
+	TestResult result;
+	result.m_countTests = testUnitQuery.size();
+	RunTest(testUnitQuery, result);
 
+	Summary(result);
+
+	// wait for user enter.
 	std::getchar();
 }
 
+/*!
+	\brief a light struct to help record error count.
+*/
 struct ErrorLogger
 {
 private:
-	size_t _errorCount = 0;
+	size_t m_errorCount = 0;
 
 public:
-	ErrorLogger() : _errorCount(0) {}
+	ErrorLogger() : m_errorCount(0) {}
 
-	inline bool conclusion()
+	inline int conclusion()
 	{
-		return _errorCount == 0;
+		return m_errorCount;
 	}
 
 	inline size_t getErrorCount() const
 	{
-		return _errorCount;
+		return m_errorCount;
 	}
 
 	// errorCount increase by one
 	inline void tick()
 	{
-		++_errorCount;
+		++m_errorCount;
 	}
 
 	// errorCount increase by one
 	inline ErrorLogger& operator ++ ()
 	{
-		++_errorCount;
+		++m_errorCount;
 		return *this;
 	}
 
@@ -197,26 +196,26 @@ public:
 	// it is same as the ++errorLogger;
 	inline ErrorLogger& operator ++ (int)
 	{
-		++_errorCount;
+		++m_errorCount;
 		return *this;
 	}
 
 	// add specific error count.
 	inline void addErrorCount(size_t newErrorCount)
 	{
-		_errorCount += newErrorCount;
+		m_errorCount += newErrorCount;
 	}
 
 	// add specific error count.
 	inline ErrorLogger& operator += (size_t newErrorCount)
 	{
-		_errorCount += newErrorCount;
+		m_errorCount += newErrorCount;
 		return *this;
 	}
 
 	inline void LogError(size_t errorCount)
 	{
-		_errorCount += errorCount;
+		m_errorCount += errorCount;
 	}
 
 	// log one error if not equal
@@ -288,7 +287,7 @@ public:
 	{
 		if (false == falseMeansOneError)
 		{
-			++_errorCount;
+			++m_errorCount;
 		}
 	}
 
@@ -297,7 +296,7 @@ public:
 	{
 		if (true == trueMeansOneError)
 		{
-			++_errorCount;
+			++m_errorCount;
 		}
 	}
 };
