bcd2582c27f98ef335d105f55472c6f8b5558c6a
刘洋 <753090084@qq.com>
2018-08-01 19:44:50 +0800

    将SuitQueueRunner转移到Suit定义的头文件中。

--- MyTools/TestTool/Suit.h
+++ MyTools/TestTool/Suit.h
@@ -157,4 +157,47 @@ public:
     }
 };
 
+
+/*!
+    \brief a helper class to Run multiple Suit at once.
+*/
+template<typename ... SUIT_TYPE_LIST>
+class SuitQueueRunner
+{
+protected:
+    std::vector<std::unique_ptr<TestSuit::SuitInterface>> m_suitQueue;
+
+public:
+    SuitQueueRunner()
+    {
+        std::unique_ptr<TestSuit::SuitInterface> list[] = { nullptr /* prevent array has size of ZERO */, std::make_unique<SUIT_TYPE_LIST>()... };
+        const int num = sizeof...(SUIT_TYPE_LIST);
+
+        for (int i = 1; i <= num; ++i)
+        {
+            m_suitQueue.push_back(std::move(list[i]));
+        }
+    }
+
+    /*!
+        \brief add on additional suit.
+        \param suit the suit you want to run.
+    */
+    void AddSuit(std::unique_ptr<TestSuit::SuitInterface> suit)
+    {
+        m_suitQueue.push_back(std::move(suit));
+    }
+
+    /*!
+        \brief fire up Suit::Start() for each suit instance.
+    */
+    void StartBatch()
+    {
+        for (auto& suit : m_suitQueue)
+        {
+            suit->Start();
+        }
+    }
+};
+
 } // namespace TestSuit
